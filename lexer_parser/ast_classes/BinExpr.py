05f52abba49b25d99f2e154cc665d3dfa82c8499
Memo Saldana <memo.sal98@gmail.com>
2022-05-08 00:26:55 -0500

    Generalized OR Expr node to binary expr node, since precedence is kept by grammar

--- lexer_parser/ast_classes/BinExpr.py
+++ lexer_parser/ast_classes/BinExpr.py
@@ -1,15 +1,16 @@
 from lexer_parser.ast_classes.Node import Node
 
-class Expr(Node):
-  def __init__(self, left, right):
+class BinExpr(Node):
+  def __init__(self, left, op, right):
     super().__init__('Expr')
     self.left = left
     self.right = right
+    self.op = op
 
   def gen():
     left.gen()
     if hasattr(self, 'right'):
       right.gen()
-      # newType = SemanticCube.get_type('OR', left, right)
+      # newType = SemanticCube.get_type(op, left, right)
       # malloc temp : newType
       # quadruple('OR', left, right, temp)
