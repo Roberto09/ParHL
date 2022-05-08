7b3cd9d74dfd95705ec53c7afe0b685425eaae5e
Memo Saldana <memo.sal98@gmail.com>
2022-05-08 00:31:13 -0500

    Adds Unary Expr node for AST

---
+++ lexer_parser/ast_classes/UnExpr.py
@@ -0,0 +1,13 @@
+from lexer_parser.ast_classes.Node import Node
+
+class UnExpr(Node):
+  def __init__(self, op, arg):
+    super().__init__('UnExpr')
+    self.op = op
+    self.arg = arg
+
+  def gen():
+    self.arg.gen()
+    # newType = SC.get_type(op, arg)
+    # malloc temp : newType
+    # quadruple(op, arg, None, temp)
