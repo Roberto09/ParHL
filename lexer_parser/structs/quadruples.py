663ead3254469e4f6550486782fb08e61ff74b7c
Memo Saldana <memo.sal98@gmail.com>
2022-05-17 18:46:08 -0500

    Sets all args and result of Quadruple as optional, for GOTOs

--- lexer_parser/structs/quadruples.py
+++ lexer_parser/structs/quadruples.py
@@ -1,6 +1,6 @@
 
 class Quadruple():
-    def __init__(self, op, arg_1, arg_2, result):
+    def __init__(self, op, arg_1=None, arg_2=None, result=None):
         self.op = op
         self.arg_1 = arg_1
         self.arg_2 = arg_2
