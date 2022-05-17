e6da80632b19509dae393ca3427bb55f39846a38
Memo Saldana <memo.sal98@gmail.com>
2022-05-17 12:29:49 -0500

    adds quadruple creation function to ctx

--- lexer_parser/structs/parse_context.py
+++ lexer_parser/structs/parse_context.py
@@ -1,7 +1,15 @@
 from .semantic_cube import SemanticCube
 from .var_dir import FuncDir
-
+from .quadruples import Quadruple
+    
 class ParseContext():
     def __init__(self):
         self.func_dir = FuncDir()
         self.semantic_cube = SemanticCube()
+        self._quadruples: list[Quadruple] = []
+
+    def get_quadruples(self):
+        return self._quadruples
+    
+    def add_quadruple(self, quadruple: Quadruple):
+        self._quadruples.append(quadruple)
